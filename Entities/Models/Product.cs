db41117de931f35d652a183c39bc4fa3979e51e7
Esforper <dolanbayomerfaruk@gmail.com>
2023-08-17 22:43:35 +0300

    9.7_Model_Validation

--- Entities/Models/Product.cs
+++ Entities/Models/Product.cs
@@ -1,9 +1,16 @@
-﻿namespace Entities.Models;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Entities.Models;
 // namespace kısmını Store.Entities gibi özelleştirebiliriz
 public class Product
 {
-     public int ProductId { get; set; }
+        public int ProductId { get; set; }
+
+        [Required(ErrorMessage = "ProductName is required")] //boş ürün tanımlamamak için
         public String? ProductName { get; set; } = string.Empty;
+
+
+        [Required(ErrorMessage = "The Value is invalid")]  //ilgili nesnenin hemen üstüne yazılmalı
         public decimal Price { get; set; }
 
 }
